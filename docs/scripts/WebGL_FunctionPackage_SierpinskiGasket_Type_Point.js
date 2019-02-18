16d740236c33ffd4a3bfd7577481c124497f95d3
hly920928 <hly920928@gmail.com>
2019-02-18 22:00:16 +0800

    WebGL_Demo_SierpinskiGasket
    Finish SierpinskiGasket Type Polygon

--- docs/scripts/WebGL_FunctionPackage_SierpinskiGasket_Type_Point.js
+++ docs/scripts/WebGL_FunctionPackage_SierpinskiGasket_Type_Point.js
@@ -1,101 +1,86 @@
-var FunctionPackage_SierpinskiGasket_Type_Point=
-{
-    configureWebGL: function(envir){
-        envir.gl.viewport( 0, 0, envir.cnv.width,  envir.cnv.height);
-        envir.gl.clearColor(0.8, 0.8, 0.8, 1.0);
-        envir.gl.clear(envir.gl.COLOR_BUFFER_BIT);
-    },
-    mainCallBackDraw:"",
-    setupUI :function(_envir){
-        //var _containerID=cantainerID;
-        //generate mainDraw function in this closure
-    var envir=_envir;
-    var fp=this;
-    this.mainCallBackDraw=function()
+function FunctionPackage_SierpinskiGasket_TypePoint_Constructor(){
+    var FunctionPackage_SierpinskiGasket_Type_Point=
     {
-        fp.getInput(envir);
-        envir.shadersProgram=configShaders(envir.gl, envir.cantainerID);
-        fp.produceGeometryData(envir);
-        fp.sendDataToGPU(envir);
-        fp.associateDataInShaders(envir);
-        fp.mainRender(envir);
-    }
-    envir.viewPortUIControler=Object.create(ButtonToHideDivProto);
-    envir.viewPortUIControler.setup(envir.cantainerID+" .btnToggleForm_viewport",envir.cantainerID+" .shaderInput",FuncPackage());
-    document.querySelector(envir.cantainerID+" .inputRangeElem").addEventListener("change",this.mainCallBackDraw);
-    document.querySelector(envir.cantainerID+" .btnUpdateShader_viewport").addEventListener("click",this.mainCallBackDraw);
-    },
-    getInput:function(envir){
-        var sliderBar=document.querySelector(envir.cantainerID+" .inputRangeElem");
-        var np=Number(sliderBar.value);   
-        var numsOfPoints=document.querySelector(envir.cantainerID+" .numsOfPoints");
-        numsOfPoints.innerText=" "+np;
-        if(np<=0){
-            envir.gl.clear(envir.gl.COLOR_BUFFER_BIT );
-            return;
-        };
-        envir.inputVar.numsOfPoinnts=np;
-    },
-    produceGeometryData:function(envir){
-        var numPoints = envir.inputVar.numsOfPoinnts;
-        var vertices = [
-        vec3(-1.0, -1.0,0.0),
-        vec3(0.0, 1.0,0.0),
-        vec3(1.0, -1.0,0.0)
-        ];
-         
-        /* 
-        
-         var vertices = [
-        vec2(-1.0, -1.0),
-        vec2(0.0, 1.0),
-        vec2(1.0, -1.0)
-        ];
-        */
-    
-        var u = mix(vertices[0], vertices[1], 0.5);
-        var v = mix(vertices[0], vertices[2], 0.5);
-        var p = mix(u, v, 0.5);
-        var points = [ p ];
-        for (var i = 0; points.length < numPoints; ++i) {
-        var j = Math.floor(Math.random() * 3);
-        p = mix(points[i], vertices[j], 0.5);
-        points.push(p);
+        configureWebGL: function(envir){
+            envir.gl.viewport( 0, 0, envir.cnv.width,  envir.cnv.height);
+            envir.gl.clearColor(0.8, 0.8, 0.8, 1.0);
+            envir.gl.clear(envir.gl.COLOR_BUFFER_BIT);
+        },
+        mainCallBackDraw:"",
+        setupUI :function(_envir){
+            //var _containerID=cantainerID;
+            //generate mainDraw function in this closure
+        var envir=_envir;
+        var fp=this;
+        this.mainCallBackDraw=function()
+        {
+            fp.getInput(envir);
+            envir.shadersProgram=configShaders(envir.gl, envir.cantainerID);
+            fp.produceGeometryData(envir);
+            fp.sendDataToGPU(envir);
+            fp.associateDataInShaders(envir);
+            fp.mainRender(envir);
         }
-        envir.dataSet.Points=points;
-    },
-    sendDataToGPU:function(envir){
-        envir.bufferId =  envir.gl.createBuffer();
-        envir.gl.bindBuffer(  envir.gl.ARRAY_BUFFER,  envir.bufferId );
-        envir.gl.bufferData(  envir.gl.ARRAY_BUFFER, flattenArrayOfVectors( envir.dataSet.Points),  envir.gl.STATIC_DRAW );
-    },
-    associateDataInShaders:function(envir){
-        var vertexPositions = envir.gl.getAttribLocation(  envir.shadersProgram, "vertexPosition" );
-        envir.gl.vertexAttribPointer( vertexPositions, 3,  envir.gl.FLOAT, envir, 0, 0 );
-        envir.gl.enableVertexAttribArray( vertexPositions );
-    },
-    mainRender:function(envir){
-        envir.gl.clear(  envir.gl.COLOR_BUFFER_BIT );
-        envir.gl.drawArrays(envir.gl.POINTS, 0, envir.inputVar.numsOfPoinnts);
+        envir.viewPortUIControler=Object.create(ButtonToHideDivProto);
+        envir.viewPortUIControler.setup(envir.cantainerID+" .btnToggleForm_viewport",envir.cantainerID+" .shaderInput",FuncPackage());
+        document.querySelector(envir.cantainerID+" .inputRangeElem").addEventListener("change",this.mainCallBackDraw);
+        document.querySelector(envir.cantainerID+" .btnUpdateShader_viewport").addEventListener("click",this.mainCallBackDraw);
+        },
+        getInput:function(envir){
+            var sliderBar=document.querySelector(envir.cantainerID+" .inputRangeElem");
+            envir.inputVar.sliderBarInput=Number(sliderBar.value);   
+            var inputDisplay1=document.querySelector(envir.cantainerID+" .inputDisplay1");
+            inputDisplay1.innerText=" "+envir.inputVar.sliderBarInput;
+            if(envir.inputVar.sliderBarInput<=0){
+                envir.gl.clear(envir.gl.COLOR_BUFFER_BIT );
+                return;
+            };
+        },
+        produceGeometryData:function(envir){
+            var numPoints = envir.inputVar.sliderBarInput;
+            var vertices = [
+            vec3(-1.0, -1.0,0.0),
+            vec3(0.0, 1.0,0.0),
+            vec3(1.0, -1.0,0.0)
+            ];
+             
+            /* 
+            
+             var vertices = [
+            vec2(-1.0, -1.0),
+            vec2(0.0, 1.0),
+            vec2(1.0, -1.0)
+            ];
+            */
+        
+            var u = mix(vertices[0], vertices[1], 0.5);
+            var v = mix(vertices[0], vertices[2], 0.5);
+            var p = mix(u, v, 0.5);
+            var points = [ p ];
+            for (var i = 0; points.length < numPoints; ++i) {
+            var j = Math.floor(Math.random() * 3);
+            p = mix(points[i], vertices[j], 0.5);
+            points.push(p);
+            }
+            envir.dataSet.Points=points;
+        },
+        sendDataToGPU:function(envir){
+            envir.bufferId =  envir.gl.createBuffer();
+            envir.gl.bindBuffer(  envir.gl.ARRAY_BUFFER,  envir.bufferId );
+            envir.gl.bufferData(  envir.gl.ARRAY_BUFFER, flattenArrayOfVectors( envir.dataSet.Points),  envir.gl.STATIC_DRAW );
+        },
+        associateDataInShaders:function(envir){
+            var vertexPositions = envir.gl.getAttribLocation(  envir.shadersProgram, "vertexPosition" );
+            envir.gl.vertexAttribPointer( vertexPositions, 3,  envir.gl.FLOAT, envir, 0, 0 );
+            envir.gl.enableVertexAttribArray( vertexPositions );
+        },
+        mainRender:function(envir){
+            envir.gl.clear(  envir.gl.COLOR_BUFFER_BIT );
+            envir.gl.drawArrays(envir.gl.POINTS, 0, envir.inputVar.sliderBarInput);
+        }
     }
+
+    return FunctionPackage_SierpinskiGasket_Type_Point;
 }
 
 
-function producePointsV2(n){
-    numPoints = n;
-   var vertices = [
-   vec3(-1.0, -1.0, 0.0),
-   vec3(0.0, 1.0, 0.0),
-   vec3(1.0, -1.0, 0.0)
-   ];
-   var u = mix(vertices[0], vertices[1], 0.5);
-   var v = mix(vertices[0], vertices[2], 0.5);
-   var p = mix(u, v, 0.5);
-   var points = [ p ];
-   for (var i = 0; points.length < numPoints; ++i) {
-   var j = Math.floor(Math.random() * 3);
-   p = mix(points[i], vertices[j], 0.5);
-   points.push(p);
-   }
-   return points;
-}
--- docs/scripts/Webgl_ SierpinskiGasket.js
+++ docs/scripts/Webgl_ SierpinskiGasket.js
@@ -1,47 +1,53 @@
 "use strict";
 var WebGLModuleController={};
-var WebGLModuleFunctionPackageProto={
-    configureWebGL:"",
-    setupUI:"",
-    getInput:"",
-    produceGeometryData:"",
-    sendDataToGPU:"",
-    associateDataInShaders:"",
-    mainCallBackDraw:"",
-    mainRender:""
-}
-var WebGLModuleControllerProto={
-   envir:{
-    cantainerID:"",
-    dataSet:{},
-    cnv:"",
-    gl:"",
-    shadersProgram:"",
-    bufferId:"",
-    inputVar:{},
-    viewPortUIControler:""
-   },
-   FunctionPackage:{
-    configureWebGL:"",
-    setupUI:"",
-    mainDraw:"",
-    getInput:"",
-    produceGeometryData:"",
-    sendDataToGPU:"",
-    associateDataInShaders:"",
-    mainRender:""
-   },
-    setup: function(_cantainerID,FunctionPackage){
-    this.FunctionPackage=FunctionPackage;
-    this.envir.cantainerID="#"+_cantainerID;
-    this.envir.cnv = document.querySelector( this.envir.cantainerID+ " .gl-canvas" );
-    this.envir.gl = this.envir.cnv.getContext( "webgl" );
-    this.FunctionPackage.configureWebGL(this.envir);
-    this.FunctionPackage.setupUI(this.envir);
-    },
-    changeFunctionPackage:function(FunctionPackage){
-        this.FunctionPackage=FunctionPackage;
+function WebGLModuleFunctionPackageConstructor() {
+    var WebGLModuleFunctionPackageProto={
+        configureWebGL:"",
+        setupUI:"",
+        getInput:"",
+        produceGeometryData:"",
+        sendDataToGPU:"",
+        associateDataInShaders:"",
+        mainCallBackDraw:"",
+        mainRender:""
     }
+    return WebGLModuleFunctionPackageProto;
+}
+function WebGLModuleControllerConstructor(_cantainerID,FunctionPackage) {
+    var WebGLModuleControllerProto={
+        envir:{
+         cantainerID:"",
+         dataSet:{},
+         cnv:"",
+         gl:"",
+         shadersProgram:"",
+         bufferId:"",
+         inputVar:{},
+         viewPortUIControler:""
+        },
+        FunctionPackage:{
+         configureWebGL:"",
+         setupUI:"",
+         mainCallBackDraw:"",
+         getInput:"",
+         produceGeometryData:"",
+         sendDataToGPU:"",
+         associateDataInShaders:"",
+         mainRender:""
+        },
+        changeFunctionPackage:function(FunctionPackage){
+            this.FunctionPackage=FunctionPackage;
+        }
+    }
+    var thisMod=WebGLModuleControllerProto;
+    //settup
+    thisMod.FunctionPackage=FunctionPackage;
+    thisMod.envir.cantainerID="#"+_cantainerID;
+    thisMod.envir.cnv = document.querySelector( thisMod.envir.cantainerID+ " .gl-canvas" );
+    thisMod.envir.gl = thisMod.envir.cnv.getContext( "webgl" );
+    thisMod.FunctionPackage.configureWebGL(thisMod.envir);
+    thisMod.FunctionPackage.setupUI(thisMod.envir);
+    return thisMod;
 }
 
 
@@ -50,10 +56,11 @@ window.onload=function init(){
    
     //document.getElementById("BTN").addEventListener("click",drawSG);
     //var ctx2D=cnv.getContext("2d");
-    WebGLModuleController["WebGLModule_1"]=Object.create(WebGLModuleControllerProto);
-    WebGLModuleController["WebGLModule_1"].setup("WebGLModule_1",FunctionPackage_SierpinskiGasket_Type_Point);
+    WebGLModuleController["WebGLModule_1"]=WebGLModuleControllerConstructor("WebGLModule_1",FunctionPackage_SierpinskiGasket_TypePoint_Constructor());
     //settingCanvas();
-    
+    WebGLModuleController["WebGLModule_2"]=WebGLModuleControllerConstructor("WebGLModule_2",FunctionPackage_SierpinskiGasket_TypePolygon_Constructor());
+    WebGLModuleController["WebGLModule_2"].FunctionPackage.mainCallBackDraw();
+
 }
 
 
